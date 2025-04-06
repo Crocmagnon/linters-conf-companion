b1a0bb1a0c89e1fc1f3a143428adc05ce742ffc6
Gabriel Augendre <gabriel.augendre@ovhcloud.com>
2025-04-06 10:58:15 +0200

    fatcontext use inspector to walk through relevant nodes

--- fatcontext/fatcontext.go
+++ fatcontext/fatcontext.go
@@ -1,15 +1,29 @@
 package fatcontext
 
 import (
+	"go/ast"
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name: "fatcontext",
-	Doc:  "detects nested contexts in loops",
-	Run:  run,
+	Name:     "fatcontext",
+	Doc:      "detects nested contexts in loops",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	inspctr := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	nodeFilter := []ast.Node{
+		(*ast.RangeStmt)(nil),
+	}
+
+	inspctr.Preorder(nodeFilter, func(node ast.Node) {
+
+	})
+
 	return nil, nil
 }
